395e2ea65e56ba11a67275e5805df42db264a13d
zjx <1193028309@qq.com>
2022-11-07 20:15:06 +0800

    完成节流 完结作业

--- 作业2：我的-设置/js/Setrem.js
+++ 作业2：我的-设置/js/Setrem.js
@@ -1,10 +1,17 @@
+var flg = true;
 function setRem() {
-  var ui_w = 375;
-  var client_w =
-    document.documentElement.clientWidth || document.body.clientWidth;
+  if (flg) {
+    flg = false;
+    var ui_w = 375;
+    var client_w =
+      document.documentElement.clientWidth || document.body.clientWidth;
 
-  var html = document.querySelector("html");
-  html.style.fontSize = (client_w / ui_w) * 10 + "px";
+    var html = document.querySelector("html");
+    html.style.fontSize = (client_w / ui_w) * 10 + "px";
+    setTimeout(function () {
+      flg = true;
+    }, 100);
+  }
 }
 
 window.onresize = setRem;
